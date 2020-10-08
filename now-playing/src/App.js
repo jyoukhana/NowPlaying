700f27324c06a3537b916d8b777945d06ae72317
John Youkhana <jyoukhana1765@gmail.com>
2020-10-08 16:15:01 -0500

    Added MaterialUI stuff and added card-style theme

--- now-playing/src/App.js
+++ now-playing/src/App.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import Spotify from 'spotify-web-api-js';
 
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+
+
 const spotifyWebAPI = new Spotify();
+
+const useStyles = theme => ({
+  root: {
+    display: 'flex',
+    maxWidth: 100,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 class App extends Component {
   constructor(){
@@ -51,29 +71,62 @@ class App extends Component {
 
   render(){
     return (
-      <div className="App">
+      // <div className="App">
+      //   <a href='http://localhost:8888'>
+      //     <button>Log in with your Spotify account</button>
+      //   </a>
+
+      //   <div>
+      //     <img src={ this.state.nowPlaying.image } style={{ width: 400 }}/>
+      //   </div>
+
+      //   <div>
+      //     Album: { this.state.nowPlaying.album }
+      //   </div>
+
+      //   <div>
+      //     Now Playing "{ this.state.nowPlaying.name }" by { this.state.nowPlaying.artists }
+      //   </div>
+
+      //   <button onClick={() => this.getNowPlaying()}>
+      //     Check song that user is now playing
+      //   </button>
+      // </div>
+
+      <div>
         <a href='http://localhost:8888'>
           <button>Log in with your Spotify account</button>
         </a>
 
-        <div>
-          <img src={ this.state.nowPlaying.image } style={{ width: 400 }}/>
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              image={this.state.nowPlaying.image}
+              title= {this.state.nowPlaying.name}
+            />
+          <CardContent>
+            <canvas id="nowPlaying" width="100" height="100"></canvas>
+            <Typography gutterBottom variant="h5" component="h2">
+              {this.state.nowPlaying.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {this.state.nowPlaying.artists}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {this.state.nowPlaying.album}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button onClick={() => this.getNowPlaying()} size="small" color="primary">
+              Now Playing
+            </Button>
+          </CardActions>
+          </CardActionArea>
+        </Card>
         </div>
-
-        <div>
-          Album: { this.state.nowPlaying.album }
-        </div>
-
-        <div>
-          Now Playing "{ this.state.nowPlaying.name }" by { this.state.nowPlaying.artists }
-        </div>
-
-        <button onClick={() => this.getNowPlaying()}>
-          Check song that user is now playing
-        </button>
-      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(useStyles)(App);
